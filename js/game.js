0afdf167c76899c1ef8354fd12cb7752ac75dd0c
Obamium3157 <p_zhdanov@icloud.com>
2022-07-10 15:53:34 +0300

    Добавлены основные функции + начата работа с физикой

--- js/game.js
+++ js/game.js
@@ -1,18 +1,47 @@
-var canvas = document.getElementById('canvas');
-var canvasContext = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const canvasContext = canvas.getContext('2d');
 
-var GAME = {
+const GAME = {
     width: 1000,
     height: 600,
     background: "gray",
 }
 
+const PLAYER = {
+    x: 20,
+    y: 20,
+    width: 50,
+    height: 80,
+    background: "red",
+}
+
 canvas.width = GAME.width;
 canvas.height = GAME.height;
 
-drawBackground();
+play();
+
+function physics() {
+    PLAYER.y += 7;
+}
 
 function drawBackground() {
     canvasContext.fillStyle = GAME.background;
     canvasContext.fillRect(0, 0, GAME.width, GAME.height);
+}
+
+function drawPlayer() {
+    canvasContext.fillStyle = PLAYER.background;
+    canvasContext.fillRect(PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height);
+}
+
+function drawFrame() {
+    canvasContext.clearRect(0, 0, GAME.width, GAME.height);
+    drawBackground();
+    drawPlayer();
+}
+
+function play() {
+    drawFrame();
+    physics();
+    requestAnimationFrame(play)
 }
